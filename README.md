94bd838ad6e7b1eb7e043b857eb3be065c088fa6
Evan Lucas <evanlucas@me.com>
2013-12-04 15:11:03 -0600

    Added tests and README

---
+++ README.md
@@ -0,0 +1,44 @@
+# mdview
+
+A very simple markdown viewer
+
+## Install
+
+```
+$ npm install -g mdview
+```
+
+## Test
+
+Requires `grunt`
+
+```
+$ npm install -g grunt-cli
+$ grunt test
+```
+
+## Usage
+
+```
+  Usage: mdview [options]
+  
+  Options:
+  
+    -h, --help           output usage information
+    -V, --version        output the version number
+    -v, --verbose        Increase verbosity
+    -p, --port <port>    Port on which to run the server (3000)
+    -c, --css <style>    Customize css
+
+```
+
+## Styles
+
+Currently, the following styles are available by default
+
+- default
+- npm
+- github
+
+One can pass in the path to another css file that will be used
+
---
+++ tests/test.js
@@ -0,0 +1,27 @@
+var request = require('supertest')
+
+process.env.MDVIEW_ENV_DIR = __dirname + '/testdir'
+
+var server = require('../index')
+
+request = request('http://localhost:3000')
+
+describe('HTTP', function() {
+  describe('Valid markdown document', function() {
+    it('Should return 200', function(done) {
+      request.get('/readme.md').expect(200, done)
+    })
+  })
+  
+  describe('Non-markdown document', function() {
+    it('Should return 500', function(done) {
+      request.get('/readme.mdf').expect(500, done)
+    })
+  })
+  
+  describe('Non-existent file', function() {
+    it('Should return 404', function(done) {
+      request.get('/test.md').expect(404, done)
+    })
+  })
+})
