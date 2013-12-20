0bbd3958d29ccd8a6ac008a8886cac8259d9eac5
Evan Lucas <evanlucas@me.com>
2013-12-20 00:59:19 -0600

    Fixed public/js/index.js
    - Fixed hashes on theme switch
    - Removed e.preventDefault() and return false on click handlers
    - Added check for hash

--- public/js/index.js
+++ public/js/index.js
@@ -1,27 +1,28 @@
 $(document).ready(function() {
   function setTheme(theme) {
+    theme = theme.replace('#', '')
     switch (theme) {
       case 'github':
         $('link[rel=stylesheet]').attr('href', '/css/github.css')
-        $('.theme-default').html('<a href="#">default</a>')
+        $('.theme-default').html('<a href="#default">default</a>')
         $('.theme-github').text('github')
-        $('.theme-npm').html('<a href="#">npm</a>')
-        $('.theme-gitlab').html('<a href="#">gitlab</a>')
+        $('.theme-npm').html('<a href="#npm">npm</a>')
+        $('.theme-gitlab').html('<a href="#gitlab">gitlab</a>')
         $('.theme-name').text('github')
         break
       case 'npm':
         $('link[rel=stylesheet]').attr('href', '/css/npm.css')
-        $('.theme-default').html('<a href="#">default</a>')
-        $('.theme-github').html('<a href="#">github</a>')
+        $('.theme-default').html('<a href="#default">default</a>')
+        $('.theme-github').html('<a href="#github">github</a>')
         $('.theme-npm').text('npm')
-        $('.theme-gitlab').html('<a href="#">gitlab</a>')
+        $('.theme-gitlab').html('<a href="#gitlab">gitlab</a>')
         $('.theme-name').text('npm')
         break
       case 'gitlab':
         $('link[rel=stylesheet]').attr('href', '/css/gitlab.css')
-        $('.theme-default').html('<a href="#">default</a>')
-        $('.theme-github').html('<a href="#">github</a>')
-        $('.theme-npm').html('<a href="#">npm</a>')
+        $('.theme-default').html('<a href="#default">default</a>')
+        $('.theme-github').html('<a href="#github">github</a>')
+        $('.theme-npm').html('<a href="#npm">npm</a>')
         $('.theme-gitlab').text('gitlab')
         $('.theme-name').text('gitlab')
         break
@@ -29,9 +30,9 @@ $(document).ready(function() {
       default:
         $('link[rel=stylesheet]').attr('href', '/css/style.css')
         $('.theme-default').text('default')
-        $('.theme-github').html('<a href="#">github</a>')
-        $('.theme-npm').html('<a href="#">npm</a>')
-        $('.theme-gitlab').html('<a href="#">gitlab</a>')
+        $('.theme-github').html('<a href="#github">github</a>')
+        $('.theme-npm').html('<a href="#npm">npm</a>')
+        $('.theme-gitlab').html('<a href="#gitlab">gitlab</a>')
         $('.theme-name').text('default')
         break
     }
@@ -45,9 +46,12 @@ $(document).ready(function() {
   ]
   themes.forEach(function(theme) {
     $('body').on('click', theme.className, function(e) {
-      e.preventDefault()
       setTheme(theme.name)
-      return false
     })
   })
+
+  var hash = window.location.hash
+  if (hash) {
+    setTheme(hash)
+  }
 })
